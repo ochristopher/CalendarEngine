539300d556b2a9df45461bb2771eb5ebb5412011
Christopher Onyedinma <christopheronyedinma@Christophers-MacBook-Pro.local>
2018-08-08 13:43:27 +0100

    relationships

--- app/models/calendar_engine/holiday.rb
+++ app/models/calendar_engine/holiday.rb
@@ -2,5 +2,11 @@ module CalendarEngine
   class Holiday < ActiveRecord::Base
     belongs_to :user, :class_name => 'User', :foreign_key => 'user_id'
     belongs_to :account
+
+    before_save :update_account_id
+
+    def update_account_id
+      self.account_id = user.account_id
+    end
   end
 end
